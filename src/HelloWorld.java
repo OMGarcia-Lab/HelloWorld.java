1bef8bd69abf43babff722550ea55c078f894090
oscar garcia <oscargarcia@oscars-MacBook-Pro.local>
2024-11-03 01:06:07 -0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My First Java Program!");
+
+        // Could also have them both in one line if you'd like! //
+        // System.out.println("Hello World! \nMy First Java Program!"); //
+    }
+}
